60e220c46792ea038c798f270d522068c5d90bc1
Sasha Brylov <S.Brylov@outlook.com>
2022-05-15 19:22:56 +0300

    Model added.

--- test-console-app/Program.cs
+++ test-console-app/Program.cs
@@ -4,6 +4,16 @@ namespace test_console_app {
   class Program {
     static void Main(string[] args) {
       Console.WriteLine("Hello World!");
+
+      var girl = new Girl {
+        Id = 1,
+        Name = "Tanya",
+        BustSize = 4,
+        Age = 40
+      };
+
+      Console.WriteLine(girl.Name);
+
     }
   }
 }
